4b4cfe6444d0dad3b066bcda9407cc3c973647b3
Jeffrey Ruder <ruderjt@gmail.com>
2016-03-19 14:26:14 -0700

    Retrieve data from Yelp API using OkHttp and Signpost

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -3,12 +3,18 @@ package com.epicodus.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
     @Bind(R.id.locationTextView) TextView mLocationTextView;
@@ -28,6 +34,32 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+
+        getRestaurants(location);
+
         mLocationTextView.setText("Here are all the restaurants near " + location);
+    }
+
+    private void getRestaurants(String location) {
+        final YelpService yelpService = new YelpService(this);
+
+        yelpService.findRestaurants(location, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        Log.v("JSON DATA", jsonData);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
